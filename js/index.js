a58da66d3295c9dcb7989b4c554a335a820cfd67
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2021-04-02 05:27:10 -0300

    la pelicula

---
+++ js/index.js
@@ -0,0 +1,28 @@
+function magicoDeOz() {
+    document.getElementById("oz").innerHTML = "Em Kansas, Dorothy (Judy Garland) vive em uma fazenda com seus tios. Quando um tornado ataca a região, ela se abriga dentro de casa. A menina e seu cachorro são carregados pelo ciclone e aterrisam na terra de Oz, caindo em cima da Bruxa Má do Leste e a matando. Dorothy é vista como uma heroína, mas o que ela quer é voltar para Kansas. Para isso, precisará da ajuda do Poderoso Mágico de Oz que mora na Cidade das Esmeraldas. No caminho, ela será ameaçada pela Bruxa Má do Oeste (Margaret Hamilton), que culpa Dorothy pela morte de sua irmã, e encontrará três companheiros: um Espantalho (Ray Bolger) que quer ter um cérebro, um Homem de Lata (Jack Haley) que anseia por um coração e um Leão covarde (Bert Lahr) que precisa de coragem. Será que o Mágico de Oz conseguirá ajudar todos eles?";
+}
+
+function dumbo() {
+    document.getElementById("dumbo").innerHTML = "Dumbo é um bebê elefante de um circo que nasceu com orelhas enormes. Após ser ridicularizado por seu grupo de amigos, ele se transforma na principal atração de seu circo quando descobre que, usando suas orelhas, é capaz de fazer o que nenhum outro elefante consegue: voar.";
+}
+
+function bailarina() {
+    document.getElementById("bailarina").innerHTML = "Uma sonhadora menina órfã toma uma atitude arriscada: fugir para Paris e realizar o sonho de ser uma grande bailarina. Lá, ela decide se passar por outra pessoa, e consegue uma vaga no Grand Opera, onde vai aprontar muitas aventuras.";
+}
+
+function cinderela() {
+    document.getElementById("cinderela").innerHTML = "Esta versão do clássico conto de fadas conta a história do sonho que se torna realidade por meio de inesquecíveis canções e personagens encantadores como ratinhos, a madrasta e a fada madrinha.";
+}
+
+function reiArthur() {
+    document.getElementById("arthur").innerHTML = "Adaptação da Disney da fábula sobre as origens humildes do Rei Arthur. Como uma criança órfã, Arthur era conhecido como Wart, que queria ajudar seu irmão adotivo Kay a ser um cavaleiro. Enquanto ajudava Kay em seu treinamento, Wart se deparou com uma cabana pertencente a Merlin, um mago desastrado, mas talentoso. Merlin faz o possível para convencer o menino que ele está destinado à grandeza e quando viaja com Kay para Londres para atender uma competição, eles descobrem que Merlin tinha razão.";
+}
+
+function brancaDeNeve() {
+    document.getElementById("branca-de-neve").innerHTML = "A rainha malvada morre de ciúmes da beleza de Branca de Neve e manda mata-la. Logo, descobre que a jovem não morreu e está morando na floresta com sete amiguinhos. A princesa então é envenenada pela rainha e só o beijo de um príncipe pode salvá-la.";
+}
+
+function coraline() {
+    document.getElementById("coraline").innerHTML = "Enquanto explora sua nova casa à noite, a pequena Coraline descobre uma porta secreta que contém um mundo parecido com o dela, porém melhor em muitas maneiras. Todos têm botões no lugar dos olhos, os pais são carinhosos e os sonhos de Coraline viram realidade por lá. Ela se encanta com essa descoberta, mas logo percebe que segredos estranhos estão em ação: uma outra mãe e o resto de sua família tentam mantê-la eternamente nesse mundo paralelo.";
+}
+    
